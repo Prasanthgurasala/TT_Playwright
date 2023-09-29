215b12097f891e0a3a9591f6fbe96c1e8fa26aed
Prasanthgurasala <prasanth.gurasala@gmail.com>
2023-09-29 21:18:09 +0100

    Tests updateded

--- functionaltest/playwright/tests/testume2e.spec.ts
+++ functionaltest/playwright/tests/testume2e.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Testum Technologies', async ({ page }) => {
   await page.goto('https://www.testumtechnologies.com/');
   await page.locator('#top-bar-wrap').getByRole('link', { name: 'Testum Technologies' }).click();
   await page.getByRole('link', { name: 'Services', exact: true }).click();
   await page.getByRole('link', { name: 'Contact' }).click();
   await page.getByRole('link', { name: 'Read More' }).click();
+  await page.getByRole('heading', { name: 'About Us' }).click();
+  await page.getByRole('link', { name: 'Who We Are' }).click();
+  await page.getByRole('link', { name: 'Our Mission' }).click();
+  await page.getByRole('link', { name: 'Our Vission' }).click();
 });
